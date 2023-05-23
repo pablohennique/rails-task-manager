100668687b6f9dc0323c2015d1ab76b9465bb3f7
Pablo Hennique <phennique@gmail.com>
2023-05-23 14:54:28 -0400

    Task manager base completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # see all tasks
+  get "tasks", to: "tasks#index"
+
+  # create new task
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
+  # singe task page
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # update a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning the db..."
+Task.destroy_all
+
+puts "Creating 20 tasks..."
+
+20.times do
+  task = Task.create(
+    title: "#{Faker::Hacker.verb} #{Faker::Hacker.noun}",
+    details: Faker::Hacker.say_something_smart,
+    completed: [true, false][rand(0..1)]
+  )
+  puts "Task with id #{task.id} was created"
+end
+
+puts "All done!"
